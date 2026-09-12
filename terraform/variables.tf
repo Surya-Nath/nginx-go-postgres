369c5a2a8ca688fae8efebdda9e0a1b3a49a2f9e
Ubuntu <ubuntu@ip-172-31-8-190.ap-south-1.compute.internal>
2026-09-12 11:18:50 +0000

    compose.prod terraform ansible postgres16

---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region"    { default = "ap-south-1" }
+variable "key_name"      { default = "week6-ansible" }
+variable "ssh_cidr"      { default = "43.204.110.218/32" }
+variable "instance_type" { default = "t3.small" }
